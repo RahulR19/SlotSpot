6f3be5cf0dc585ebb9801162ceadcbde068d867e
Rahul R <31767308+RahulR19@users.noreply.github.com>
2020-04-01 15:26:44 +0530

    Update settings.py

--- fablab/settings.py
+++ fablab/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#^q1b-y*3@89m_boc%lmv*0uduh_o*dj@s^f+2ft(4o8up8-=%'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
